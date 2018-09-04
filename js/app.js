6f5286e28b1552fc7c556442a1a227e33f0f158e
Alberto Jaime Le Baron <jimmleb178@gmail.com>
2018-09-04 16:16:10 -0500

    Setup needed prototypes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function (posX, posY, velocity) {
+const Enemy = function (posX, posY, velocity) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = posX;
@@ -19,6 +19,7 @@ Enemy.prototype.update = function (dt) {
     // all computers.
     this.x += this.vel * dt; // position = velocity * time
 
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -29,23 +30,37 @@ Enemy.prototype.render = function () {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const Player = function (posX, posY, velocity) {
+    // Just like Enemy, we assign a position and velocity for the player
+    this.x = posX;
+    this.y = posY;
+    this.vel = velocity;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function () {};
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function () {};
+
+    // Now instantiate your objects.
+    // Place all enemy objects in an array called allEnemies
+    // Place the player object in a variable called player
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
+    // This listens for key presses and sends the keys to your
+    // Player.handleInput() method. You don't need to modify this.
+    document.addEventListener('keyup', function (e) {
+        var allowedKeys = {
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down'
+        };
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function (e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
-});
+        player.handleInput(allowedKeys[e.keyCode]);
+    });
