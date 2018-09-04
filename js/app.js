81d65a3015c5bb8165721d5d35d7cac45a98012f
Alberto Jaime Le Baron <jimmleb178@gmail.com>
2018-09-04 15:55:35 -0500

    updated enemy variable

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function (posX, posY, velocity) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = posX;
+    this.y = posY;
+    this.vel = velocity;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -10,14 +13,16 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.vel * dt; // position = velocity * time
+
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -34,7 +39,7 @@ Enemy.prototype.render = function() {
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
