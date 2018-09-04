bbc702d864a38f4efd6852b32cc40d205ffcb143
Alberto Jaime Le Baron <jimmleb178@gmail.com>
2018-09-04 16:31:56 -0500

    Player position established

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ Player.prototype.handleInput = function () { };
 // Place the player object in a variable called player
 let allEnemies = []; 
 
-let player = new Player();
+let player = new Player(200, 300, 50);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
